938a1b167032a0f728c6b8878110536af4839870
Marco Ferreira <marcoasf.com@gmail.com>
2022-10-12 14:49:09 -0500

    added background image tester

--- server.js
+++ server.js
@@ -44,3 +44,18 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+ //  API DAILY STOCK OPEN /CLOSE
+// const stockOptions = {
+//	method: 'GET',
+//	headers: {
+//		'X-RapidAPI-Key': 'WW20GXBNI408HQN8',
+//		'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+//	}
+//};
+//
+//fetch('https://alpha-vantage.p.rapidapi.com/query?interval=5min&function=TIME_SERIES_INTRADAY&symbol=MSFT&datatype=json&output_size=compact', stockOptions)
+//	.then(response => response.json())
+//	.then(response => console.log(response))
+//	.catch(err => console.error(err));
+//
